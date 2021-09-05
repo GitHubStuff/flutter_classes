3cd41981856fe0570392521bbc519eeda7b17f7f
Steven Smith <github@icodeforyou.com>
2021-09-04 23:56:11 -0400

    upped version

--- lib/flutter_classes.dart
+++ lib/flutter_classes.dart
@@ -1,5 +1,5 @@
 library flutter_classes;
 
-export 'source/extensions/date_time_extensions.dart';
 export 'source/abstract/observing_stateful_widget.dart';
 export 'source/classes/simple_bloc_observer.dart';
+export 'source/extensions/date_time_extensions.dart';
